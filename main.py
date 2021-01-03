91188d51d7bde9d52aeb60fb063e815dcff466de
Akronoskos <33528348+ThomasDearth@users.noreply.github.com>
2021-01-03 00:36:44 -0500

    Added !addme
    NEW COMMAND: !addme posts a link to add the bot to a server.

--- main.py
+++ main.py
@@ -18,7 +18,10 @@ def clean(message):
 
 # Returns documentation as a message in the channel
 def help_message(message_content, message_author):
-    return " Hi! I'm a dice-rolling bot for your discord servers!\nBasic commands:\n`!help`\tdisplays this page\n`!github`\tPosts a link to the GitHub page where the source code is posted\n\nSimple dice rolls:\n`!roll N`\t rolls *N* six-sided dice\n`!roll dN`\t rolls one die with *N* sides\n`!roll XdY`\t rolls *X* dice with *Y* sides each\n\nSoulbound-specific tools:\n`!roll [dice] tX`\tSpecifies the target number (x) a die must meet\n`!roll [dice] fX tY`\tSpecifies the target number that must be met (X), with Y focus added to the result\n`!roll [dice] X:Y [fZ]` Performs a check for X successes of DN X:Y. The term `fZ` can optionally included to apply Z focus to the roll."
+    return " Hi! I'm a dice-rolling bot for your discord servers!\nBasic commands:\n`!help`\tdisplays this page\n`!addme`\tPosts a link to add this bot to another server\n`!github`\tPosts a link to the GitHub page where the source code is posted\n\nSimple dice rolls:\n`!roll N`\t rolls *N* six-sided dice\n`!roll dN`\t rolls one die with *N* sides\n`!roll XdY`\t rolls *X* dice with *Y* sides each\n\nSoulbound-specific tools:\n`!roll [dice] tX`\tSpecifies the target number (x) a die must meet\n`!roll [dice] fX tY`\tSpecifies the target number that must be met (X), with Y focus added to the result\n`!roll [dice] X:Y [fZ]` Performs a check for X successes of DN X:Y. The term `fZ` can optionally included to apply Z focus to the roll."
+
+def add_me_message(message_content, message_author):
+    return "Click the link below to add me to a server of your own!\nhttps://discord.com/api/oauth2/authorize?client_id=794340963979493376&permissions=2048&scope=bot"
 
 # Posts a link to this GitHub repository
 def github_link(message_content, message_author):
@@ -42,6 +45,7 @@ async def on_message(message):
         cases = {
             "!roll" : command_modules.roll.roll_message_parse,
             "!help" : help_message,
+            "!addme" : add_me_message,
             "!github" : github_link
         }
         if message.content.split(" ")[0] not in cases:
